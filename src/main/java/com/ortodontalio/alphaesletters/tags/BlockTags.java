7e1c3e3af65cb2434f836640f98486f5570ae2ef
Ortodontalio <93380024+Ortodontalio@users.noreply.github.com>
2022-07-29 21:02:15 +0300

    Migrate to 1.19

--- src/main/java/com/ortodontalio/alphaesletters/tags/BlockTags.java
+++ src/main/java/com/ortodontalio/alphaesletters/tags/BlockTags.java
@@ -1,12 +1,12 @@
 package com.ortodontalio.alphaesletters.tags;
 
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class BlockTags {
 
-    public static final Tag<Block> LETTER_BLOCKS = TagFactory.BLOCK.create(
+    public static final TagKey<Block> LETTER_BLOCKS = TagKey.of(Registry.BLOCK_KEY,
             new Identifier("alphaesletters", "letter_blocks"));
 }
