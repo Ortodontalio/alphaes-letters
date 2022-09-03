2b3f4a0eb6ff62a681d43f74c1dde8229ec287f6
Ortodontalio <93380024+Ortodontalio@users.noreply.github.com>
2022-09-03 23:13:54 +0300

    Make strikethrough block waterlogged

--- src/main/java/com/ortodontalio/alphaesletters/common/StrikethroughBlock.java
+++ src/main/java/com/ortodontalio/alphaesletters/common/StrikethroughBlock.java
@@ -2,17 +2,25 @@ package com.ortodontalio.alphaesletters.common;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class StrikethroughBlock extends Block {
+@SuppressWarnings("deprecation")
+public class StrikethroughBlock extends Block implements Waterloggable {
 
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public StrikethroughBlock() {
         super(FabricBlockSettings
@@ -20,10 +28,10 @@ public class StrikethroughBlock extends Block {
                 .strength(1.0f, 1.0f)
                 .sounds(BlockSoundGroup.WOOD)
                 .nonOpaque());
+        setDefaultState(stateManager.getDefaultState().with(WATERLOGGED, false));
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return switch(state.get(FACING)) {
             case SOUTH -> Block.createCuboidShape(0,0,0,16,16,1);
@@ -35,12 +43,30 @@ public class StrikethroughBlock extends Block {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, WATERLOGGED);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return Boolean.TRUE.equals(state.get(WATERLOGGED))
+                ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+                                                WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (Boolean.TRUE.equals(state.get(WATERLOGGED))) {
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING,ctx.getPlayerFacing().getOpposite());
+        return this.getDefaultState()
+                .with(FACING,ctx.getPlayerFacing().getOpposite())
+                .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
     }
 
 
