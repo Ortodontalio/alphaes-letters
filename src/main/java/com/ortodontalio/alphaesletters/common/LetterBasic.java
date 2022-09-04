f6ea3d0e96475bae51b5fb54de9c983eb889da3d
Ortodontalio <93380024+Ortodontalio@users.noreply.github.com>
2022-09-04 22:35:54 +0300

    Make numbers three-dimensional

--- src/main/java/com/ortodontalio/alphaesletters/common/LetterBasic.java
+++ src/main/java/com/ortodontalio/alphaesletters/common/LetterBasic.java
@@ -1,30 +1,65 @@
 package com.ortodontalio.alphaesletters.common;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class LetterBasic extends Block {
 
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final BooleanProperty LIT = Properties.LIT;
 
     public LetterBasic() {
         super(FabricBlockSettings
-                .of(Material.STONE)
+                .of(Material.STONE, MapColor.BLUE)
                 .strength(5.0f, 10.0f)
                 .sounds(BlockSoundGroup.STONE)
+                .luminance(state -> Boolean.TRUE.equals(state.get(LIT)) ? 10 : 0)
                 .requiresTool());
+        this.setDefaultState(getDefaultState().with(LIT, false));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, LIT);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return switch(state.get(FACING)) {
+            case SOUTH -> Block.createCuboidShape(0,0,0,16,16,15);
+            case WEST -> Block.createCuboidShape(1,0,0,16,16,16);
+            case EAST -> Block.createCuboidShape(0,0,0,15,16,16);
+            default -> Block.createCuboidShape(0,0,1,16,16,16);
+        };
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack inHand = player.getStackInHand(hand);
+        if(inHand.isOf(Items.GLOWSTONE_DUST) && Boolean.FALSE.equals(state.get(LIT))) {
+            inHand.decrement(1);
+            world.setBlockState(pos, state.with(LIT, true), Block.NOTIFY_ALL);
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
     }
 
     @Override
