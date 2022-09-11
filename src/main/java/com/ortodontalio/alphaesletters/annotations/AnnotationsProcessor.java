173d29ed4863f8ef5274650c65fc1b918d5c23c4
Ortodontalio <93380024+Ortodontalio@users.noreply.github.com>
2022-09-11 15:04:07 +0300

    Replace registrations by native code

--- src/main/java/com/ortodontalio/alphaesletters/annotations/AnnotationsProcessor.java
+++ src/main/java/com/ortodontalio/alphaesletters/annotations/AnnotationsProcessor.java
@@ -5,9 +5,13 @@ import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.atteo.classindex.ClassIndex;
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.logging.Level;
-
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.logging.Logger;
@@ -20,6 +24,11 @@ import java.util.logging.Logger;
 public final class AnnotationsProcessor {
 
     private static final Logger LOGGER = Logger.getLogger(AnnotationsProcessor.class.getName());
+    private static final char PACKAGE_SEPARATOR = '.';
+    private static final char DIRECTORY_SEPARATOR = '/';
+    private static final String CLASS_SUFFIX = ".class";
+    private static final String SCANNED_PACKAGE = "com.ortodontalio.alphaesletters";
+    private static final String[] EXCLUSIONS = {"mixin","AlphaesLetters.class"};
 
     private AnnotationsProcessor(){}
 
@@ -27,7 +36,7 @@ public final class AnnotationsProcessor {
      * Method for registers {@link Block} objects, defined in classes, annotated by {@link BlockRegistrator}.
      */
     public static void registerBlocks() {
-        Iterable<Class<?>> iterable = ClassIndex.getAnnotated(BlockRegistrator.class);
+        Iterable<Class<?>> iterable = getAnnotated(BlockRegistrator.class);
         for(Class<?> className : iterable) {
             List<Field> fields = List.of(className.getDeclaredFields());
             for(Field field : fields) {
@@ -46,7 +55,7 @@ public final class AnnotationsProcessor {
      * Method for registers {@link BlockItem} objects, defined in classes, annotated by {@link BlockItemRegistrator}.
      */
     public static void registerBlockItems() {
-        Iterable<Class<?>> iterable = ClassIndex.getAnnotated(BlockItemRegistrator.class);
+        Iterable<Class<?>> iterable = getAnnotated(BlockItemRegistrator.class);
         for(Class<?> className : iterable) {
             List<Field> fields = List.of(className.getDeclaredFields());
             for(Field field : fields) {
@@ -61,4 +70,52 @@ public final class AnnotationsProcessor {
         }
     }
 
+    /**
+     * Secondary util method, uses recursion for founding necessary classes.
+     * @param file is folder of class for searching the annotation.
+     * @param scannedPackage is a package, in which folder or class is stored.
+     * @param annotation is the annotation whose classes need to be found.
+     * @return found classes' list.
+     */
+    private static List<Class<?>> find(File file, String scannedPackage, Class<? extends Annotation> annotation) {
+        List<Class<?>> classes = new ArrayList<>();
+        String resource = scannedPackage + PACKAGE_SEPARATOR + file.getName();
+        if (file.isDirectory()) {
+            for (File child : Objects.requireNonNull(file.listFiles())) {
+                classes.addAll(find(child, resource, annotation));
+            }
+        } else {
+            if (resource.endsWith(CLASS_SUFFIX)) {
+                try {
+                    Class<?> clazz = Class.forName(resource.substring(0, resource.length() - CLASS_SUFFIX.length()));
+                    if(clazz.isAnnotationPresent(annotation)) {
+                        classes.add(clazz);
+                    }
+                } catch (ClassNotFoundException ignore) {
+                    LOGGER.warning("One or more classes haven't been found.");
+                }
+            }
+        }
+        return classes;
+    }
+
+    /**
+     * Util method for searching annotated classes.
+     * @param annotation is the annotation whose classes need to be found.
+     * @return found classes' list.
+     */
+    private static List<Class<?>> getAnnotated(Class<? extends Annotation> annotation) {
+        String scannedPath = SCANNED_PACKAGE.replace(PACKAGE_SEPARATOR, DIRECTORY_SEPARATOR);
+        URL url = Thread.currentThread().getContextClassLoader().getResource(scannedPath);
+        File directory = new File(Objects.requireNonNull(url).getFile());
+        List<Class<?>> classes = new ArrayList<>();
+        List<File> files = Arrays.stream(Objects.requireNonNull(directory.listFiles()))
+                .filter(f -> Arrays.stream(EXCLUSIONS).noneMatch(s -> s.equals(f.getName())))
+                .toList();
+        for (File file : files) {
+            classes.addAll(find(file, SCANNED_PACKAGE, annotation));
+        }
+        LOGGER.log(Level.INFO, "Registrators found: {0}", classes);
+        return classes;
+    }
 }
